b3c04fa0e2e74473a9af83eeb8aa25d2a855c760
VitaliiDuduka <122724379+VitaliiDuduka@users.noreply.github.com>
2023-05-28 21:48:24 +0200

    HW07 JS task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesGallery = document.querySelector(".gallery");
+
+const markupImages = galleryItems
+  .map((image) => {
+    return (
+      '<li class="gallery__item"><a class="gallery__link" href="' +
+      image.original +
+      '"><img class="gallery__image" src="' +
+      image.preview +
+      '" alt="' +
+      image.description +
+      '"/></a></li>'
+    );
+  })
+  .join("");
+
+imagesGallery.insertAdjacentHTML("beforeend", markupImages);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+  disableScroll: false,
+});
+lightbox.on("show.lightbox", function (event) {
+  event.preventDefault();
+});
